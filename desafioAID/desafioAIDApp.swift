a9ff069f294fa7172343d949c9f9645bd32d88ef
Matheus Azevedo <matheus.klementt@gmail.com>
2021-06-25 12:23:04 -0300

    Initial Commit

---
+++ desafioAID/desafioAIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  desafioAIDApp.swift
+//  desafioAID
+//
+//  Created by Matheus Azevedo on 25/06/21.
+//
+
+import SwiftUI
+
+@main
+struct desafioAIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
