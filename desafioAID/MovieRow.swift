e6ad1911486afd78f88367e4d9ca59bbbf1aedaf
Matheus Azevedo <matheus.klementt@gmail.com>
2021-06-25 13:55:19 -0300

    Creating MovieRow view

---
+++ desafioAID/MovieRow.swift
@@ -0,0 +1,25 @@
+//
+//  MovieRow.swift
+//  desafioAID
+//
+//  Created by Matheus Azevedo on 25/06/21.
+//
+
+import SwiftUI
+
+struct MovieRow: View {
+    var movie : Movie
+    
+    var body: some View {
+        HStack {
+            Text(movie.title)
+        }
+    }
+}
+
+struct MovieRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let previewMovie = Movie(posterPath: nil, title: "Nome do filme", releaseDate: "2021", overview: "Resumo do filme", genresId: [])
+        MovieRow(movie: previewMovie)
+    }
+}
